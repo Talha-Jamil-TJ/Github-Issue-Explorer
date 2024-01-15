d58215cc361b922163771bac18f21a51c5886ff3
Talha Khan <sufi.tj114@gmail.com>
2024-01-15 12:48:40 +0100

    Chore(CI): Add Prettier to CI pipeline and pre-commit hook
    Signed-off-by: Talha Khan <sufi.tj114@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -14,9 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome github-issue-explorer'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome github-issue-explorer');
   });
 
   it(`should have as title 'github-issue-explorer'`, () => {
